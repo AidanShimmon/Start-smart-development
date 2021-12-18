674ab5c3f4fdbdcde1a0a06a536a5f4ccdc3424d
AidanShimmon <aidanshimmon@hotmail.co.uk>
2021-12-18 12:18:15 +0000

    project structure & setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,69 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.startsmartdevelopment.com",
     title: "Start Smart Development",
+    description: 'Helping new businesses create websites built to suit there needs.',
+    author: '@aidanshimmon',
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-sass`,
       options: {
-        icon: "src/images/icon.png",
+        additionalData: `@use 'variables' as *;`,
+        sassOptions: {
+          includePaths: [`${__dirname}/src/styles`],
+        }
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
-        name: "images",
-        path: "./src/images/",
+        fonts: [
+          `Raleway\: 400,700`, 
+          `Sarabun\: 600,700`,
+        ],
+        display: `swap`,
       },
-      __key: "images",
+    }, 
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `./src/images/`,
+      },
+      __key: `images`,
+    },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /assets/
+        },
+      },
+    }, 
+    {
+      resolve: 'gatsby-plugin-accessibilityjs',
+      options: {
+        injectStyles: false,
+        errorClassName: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Start Smart Development`,
+        short_name: `StartSmartDev`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
     },
   ],
 };
---
+++ src/components/global/header/header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
+
+import * as styles from "./header.module.scss"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header;  
