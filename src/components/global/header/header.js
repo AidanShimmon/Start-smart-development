c6111dd875bcd1985e2c8a85390edb2fcce0ed46
AidanShimmon <aidanshimmon@hotmail.co.uk>
2022-10-13 20:30:54 +0100

    media query adjustment, nav update

--- src/components/global/header/header.js
+++ src/components/global/header/header.js
@@ -14,7 +14,7 @@ const Header = () => {
                 </div> 
                 
                 <div>
-                    <Link href="#home">Home</Link>
+                    {/* <Link href="#home">Home</Link> */}
                     <Link href="#about">About</Link>
                     <Link href="#services">Services</Link>
                     <Button
